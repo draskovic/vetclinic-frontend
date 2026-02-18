fba22d24168476c93ceef7c16789b14a8d9b5b31
Nenad Drašković <draskovic.nenad@.gmail.com>
2026-02-18 13:24:34 +0100

    Refactor OwnerModal: reorganize fields into 3 rows

--- src/pages/owners/OwnerModal.tsx
+++ src/pages/owners/OwnerModal.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { Modal, Form, Input, Button } from 'antd';
+import { Modal, Form, Input, Button, Row, Col } from 'antd';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { ownersApi } from '@/api/owners';
 import type { Owner, CreateOwnerRequest, UpdateOwnerRequest } from '@/types';
@@ -65,53 +65,73 @@ export default function OwnerModal({ open, owner, onClose }: OwnerModalProps) {
       destroyOnHidden
     >
       <Form form={form} layout='vertical' onFinish={handleSubmit} style={{ marginTop: 16 }}>
-        <Form.Item
-          name='firstName'
-          label='Ime'
-          rules={[{ required: true, message: 'Unesite ime!' }]}
-        >
-          <Input placeholder='Ime' />
-        </Form.Item>
+        {/* Red 1: Ime, Prezime, Telefon */}
+        <Row gutter={12}>
+          <Col span={8}>
+            <Form.Item
+              name='firstName'
+              label='Ime'
+              rules={[{ required: true, message: 'Unesite ime!' }]}
+            >
+              <Input placeholder='Ime' />
+            </Form.Item>
+          </Col>
+          <Col span={8}>
+            <Form.Item
+              name='lastName'
+              label='Prezime'
+              rules={[{ required: true, message: 'Unesite prezime!' }]}
+            >
+              <Input placeholder='Prezime' />
+            </Form.Item>
+          </Col>
+          <Col span={8}>
+            <Form.Item
+              name='phone'
+              label='Telefon'
+              rules={[{ required: true, message: 'Unesite telefon!' }]}
+            >
+              <Input placeholder='+381...' />
+            </Form.Item>
+          </Col>
+        </Row>
 
-        <Form.Item
-          name='lastName'
-          label='Prezime'
-          rules={[{ required: true, message: 'Unesite prezime!' }]}
-        >
-          <Input placeholder='Prezime' />
-        </Form.Item>
+        {/* Red 2: Email, Grad, Adresa */}
+        <Row gutter={12}>
+          <Col span={8}>
+            <Form.Item
+              name='email'
+              label='Email'
+              rules={[{ type: 'email', message: 'Unesite ispravnu email adresu!' }]}
+            >
+              <Input placeholder='email@example.com' />
+            </Form.Item>
+          </Col>
+          <Col span={8}>
+            <Form.Item name='city' label='Grad'>
+              <Input placeholder='Grad' />
+            </Form.Item>
+          </Col>
+          <Col span={8}>
+            <Form.Item name='address' label='Adresa'>
+              <Input placeholder='Adresa' />
+            </Form.Item>
+          </Col>
+        </Row>
 
-        <Form.Item
-          name='phone'
-          label='Telefon'
-          rules={[{ required: true, message: 'Unesite telefon!' }]}
-        >
-          <Input placeholder='+381...' />
-        </Form.Item>
-
-        <Form.Item
-          name='email'
-          label='Email'
-          rules={[{ type: 'email', message: 'Unesite ispravnu email adresu!' }]}
-        >
-          <Input placeholder='email@example.com' />
-        </Form.Item>
-
-        <Form.Item name='city' label='Grad'>
-          <Input placeholder='Grad' />
-        </Form.Item>
-
-        <Form.Item name='address' label='Adresa'>
-          <Input placeholder='Adresa' />
-        </Form.Item>
-
-        <Form.Item name='personalId' label='JMBG'>
-          <Input placeholder='JMBG' />
-        </Form.Item>
-
-        <Form.Item name='note' label='Napomena'>
-          <Input.TextArea placeholder='Napomena...' rows={3} />
-        </Form.Item>
+        {/* Red 3: JMBG, Napomena */}
+        <Row gutter={12}>
+          <Col span={8}>
+            <Form.Item name='personalId' label='JMBG'>
+              <Input placeholder='JMBG' />
+            </Form.Item>
+          </Col>
+          <Col span={16}>
+            <Form.Item name='note' label='Napomena'>
+              <Input.TextArea placeholder='Napomena...' rows={3} />
+            </Form.Item>
+          </Col>
+        </Row>
 
         <Form.Item style={{ marginBottom: 0, textAlign: 'right' }}>
           <Button onClick={onClose} style={{ marginRight: 8 }}>
